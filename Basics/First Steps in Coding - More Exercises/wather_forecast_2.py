5b2818d6013f9656aef96b3a769e39f1e879240e
milushev91 <mmilushev91@gmail.com>
2025-12-16 16:40:55 +0200

    Create wather_forecast_2.py

---
+++ Basics/First Steps in Coding - More Exercises/wather_forecast_2.py
@@ -0,0 +1,18 @@
+degrees = float(input())
+
+if degrees < 5 or degrees > 35:
+    print("unknown")
+    
+elif degrees < 12:
+    print("Cold")
+    
+elif degrees < 15:
+    print("Cool")
+    
+elif degrees <= 20:
+    print("Mild")
+    
+elif degrees < 26:
+    print("Warm")
+else:
+    print("Hot")
