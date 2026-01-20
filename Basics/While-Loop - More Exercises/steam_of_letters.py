7cfcc6ebf9e6730d8f3d91b5999875a28ae2a440
milushev91 <mmilushev91@gmail.com>
2026-01-20 12:54:27 +0200

    Create steam_of_letters.py

---
+++ Basics/While-Loop - More Exercises/steam_of_letters.py
@@ -0,0 +1,50 @@
+command = input()
+
+output = ""
+command_letters = {
+    "c": 0,
+    "o": 0,
+    "n": 0,
+}
+
+
+while command != "End":
+    letter = command
+    
+    if letter.isalpha():
+        
+        if letter in "con":
+            
+            if command_letters[letter] == 1:
+                command_letters_count = 0
+                
+                for command_letter in command_letters:
+                    command_letters_count += command_letters[command_letter]
+                
+                if command_letters_count == 3:
+                    print(f"{output} ", end="")
+                    
+                    for command_letter in command_letters:
+                        command_letters[command_letter] = 0
+                    output = ""
+                else:
+                    output += letter
+                
+            else:
+                command_letters[letter] = 1
+                
+                command_letters_count = 0
+                for command_letter in command_letters:
+                    command_letters_count += command_letters[command_letter]
+                    
+                if command_letters_count == 3:
+                    print(f"{output} ", end="")
+                    
+                    for command_letter in command_letters:
+                        command_letters[command_letter] = 0
+                    output = ""
+        else:
+            output += letter 
+    
+        
+    command = input()
