7a3666be4bcf9100e65e4abf153380edb973d62b
milushev91 <mmilushev91@gmail.com>
2026-01-30 14:58:50 +0200

    Create tennis_ranklist.py

---
+++ Basics/Programming Basics Online Example Exam 1/tennis_ranklist.py
@@ -0,0 +1,23 @@
+tournaments_count = int(input())
+start_points = int(input())
+
+stage_points = {
+    "W": 2000,
+    "F": 1200,
+    "SF": 720,
+}
+wins = points = 0
+
+for _ in range(tournaments_count):
+    stage = input()
+    points += stage_points[stage]
+    if stage == "W":
+        wins += 1
+
+final_points = start_points + points
+avg_points = int(points / tournaments_count)
+wins_percent = wins / tournaments_count * 100
+
+print(f"Final points: {final_points}")
+print(f"Average points: {avg_points}")
+print(f"{wins_percent:.2f}%")
