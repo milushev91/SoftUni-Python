2965c72d205b540031a60e581730bb1ca3e831fa
milushev91 <mmilushev91@gmail.com>
2026-02-20 11:52:03 +0200

    Create centuries_to_minutes.py

---
+++ Fundamentals/Data Types and Variables - Lab/centuries_to_minutes.py
@@ -0,0 +1,8 @@
+DAYS_IN_YEAR = 365.2422 
+centuries = int(input())
+years = centuries * 100
+days = int(years * DAYS_IN_YEAR)
+hours = days * 24
+minutes = hours * 60
+
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
